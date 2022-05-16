cbe3d1e74ea2d04dc1575fa697ed3cb14eff189c
Bob D <txcavscout@live.com>
2022-05-15 23:46:21 -0700

    Add project files.

---
+++ MethodPractice/MyConsoleMessages.cs
@@ -0,0 +1,69 @@
+﻿namespace MethodPractice
+{
+    public static class MyConsoleMessages
+    {
+
+
+        public static string GetFirstName()
+        {
+            Console.Write("Enter your First Name: ");
+            var firstName = Console.ReadLine();
+
+            while (string.IsNullOrEmpty(firstName))
+            {
+                Console.WriteLine(":::horrified whisper::: There's nobody here. \n!?! Hello...\n");
+                Console.Write("First Name can't be empty! Enter your First Name: ");
+                firstName = Console.ReadLine();
+            }
+
+            return firstName;
+        }
+
+        public static string GetLastName()
+        {
+            Console.Write("Enter your last name: ");
+            var lastName = Console.ReadLine();
+
+            while (string.IsNullOrEmpty(lastName))
+            {
+                Console.WriteLine(":::horrified whisper::: There's nobody here. \n!?! Hello...\n");
+                Console.Write("Last Name can't be empty! Enter your Last Name: ");
+                lastName = Console.ReadLine();
+            }
+
+            return lastName;
+        }
+
+        public static double GetX()
+        {
+            string? textX = "";
+            bool isValid = false;
+            double X = 0;
+
+            while (isValid == false || string.IsNullOrEmpty(textX))
+            {
+                Console.Write("Enter a Number: ");
+                textX = Console.ReadLine();
+                isValid = double.TryParse(textX, out  X);
+            }
+
+            return X;
+        }
+
+        public static double GetY()
+        {
+            string? textY = "";
+            bool isValid = false;
+            double Y = 0;
+
+            while (isValid == false || string.IsNullOrEmpty(textY))
+            {
+                Console.Write("Enter a Number: ");
+                textY = Console.ReadLine();
+                isValid = double.TryParse(textY, out Y);
+            }
+
+            return Y;
+        }
+    }
+}
---
+++ MethodPractice/Program.cs
@@ -0,0 +1,12 @@
+﻿using MethodPractice;
+
+
+string yourFirstName = MyConsoleMessages.GetFirstName();
+string yourLastName = MyConsoleMessages.GetLastName();
+
+double firstNumber = MyConsoleMessages.GetX();
+double secondNumber = MyConsoleMessages.GetY();
+
+Console.WriteLine($"\nHello {yourFirstName} {yourLastName}, how are you today?");
+
+Console.WriteLine($"{firstNumber} + {secondNumber} = {firstNumber + secondNumber}");
